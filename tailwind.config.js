133a68640151b0fcc2013f0a553ac1858468b534
Zain <zainjaved965@gmail.com>
2023-02-14 18:46:31 +0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        grey: "#545E75"
+      },
+      fontFamily: {
+        poppins : "'poppins', sans-serif"
+      }
+    },
+  },
+  plugins: [],
+}
